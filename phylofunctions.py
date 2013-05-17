535cf27e98b6149c445e53966334fce4f2f4cb27
Rob Lanfear <rob.lanfear@gmail.com>
2013-05-17 12:28:20 +1000

    updated get_summary because to attribute S. Greenhill

--- phylofunctions.py
+++ phylofunctions.py
@@ -1029,7 +1029,40 @@ def get_taxid(term):
 
 
 def get_summary(query, database, rettype):
-	#this is copied from Simon Greenhill's get_accession function, see get_accession for copyright, above
+	#Part of this function was taken from simon greenhill's work, with modification
+	#here's an attribution to him:
+	#!/usr/bin/env python
+	# A short script to download nucleotide sequences from genbank.
+	#
+	# Copyright (c) 2009, Simon J. Greenhill <simon@simon.net.nz>
+	# All rights reserved.
+	#
+	# Redistribution and use in source and binary forms, with or without
+	# modification, are permitted provided that the following conditions
+	# are met:
+	#
+	# 1. Redistributions of source code must retain the above copyright notice,
+	#    this list of conditions and the following disclaimer.
+	#
+	# 2. Redistributions in binary form must reproduce the above copyright
+	#    notice, this list of conditions and the following disclaimer in the
+	#    documentation and/or other materials provided with the distribution.
+	#
+	# 3. Neither the name of genbank-download nor the names of its contributors
+	#    may be used to endorse or promote products derived from this software
+	#    without specific prior written permission.
+	#
+	# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+	# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	# POSSIBILITY OF SUCH DAMAGE.
 	import urllib
 	_toolname = 'get_summary'
 	_email = "rob.lanfear@gmail.com"
@@ -1295,6 +1328,7 @@ def get_longest_accession(taxid, names_list, genome):
 def get_lineage(TaxID):
     '''
     This function takes as input a genbank taxonID and returns a taxomony dictionary
+    It only works if the TaxID is at the species level
     '''
     import urllib2
     import re
